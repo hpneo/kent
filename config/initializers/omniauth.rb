11168ff03745923de91f7025b7dd56d417da1d84
HPNeo <hpneo@hotmail.com>
2013-05-25 23:27:44 -0500

    Sign in with Twitter

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :gplus, '675569114873.apps.googleusercontent.com', 'CrPACCX1RROpfVtYkZt50Cxb', scope: 'email,profile'
+
+  if Rails.env.development?
+    provider :twitter 'w6tL3GXJJ2K9FEoLe9joWg', 'UNN5XS31U5Hb6jTtP9D2dAHmVWsM7HwhuJmBdZ20w'
+  else
+    provider :twitter, '8im91zCgJ1jaTQJ9152SYg', 'w9YQ84ELW7SjrtTvSW02qHoPxPHiKxQxIYpLnOpTJ8'
+  end
 end
