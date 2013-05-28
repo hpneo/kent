003ff8e409f74bea85850c6584b70d56c0c4b041
HPNeo <hpneo@hotmail.com>
2013-05-28 12:02:03 -0500

    Add Facebook and remove timeoutable from User

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :gplus, '675569114873.apps.googleusercontent.com', 'CrPACCX1RROpfVtYkZt50Cxb', scope: 'email,profile'
+  provider :facebook, '517445554981885', '5ccd97524bb376692e3aad9f6e4b7c74', scope: 'email'
 
   if Rails.env.development?
     provider :twitter, 'w6tL3GXJJ2K9FEoLe9joWg', 'UNN5XS31U5Hb6jTtP9D2dAHmVWsM7HwhuJmBdZ20w'
