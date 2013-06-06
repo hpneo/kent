7f3f89a0c976bd8115ede67ea86bbf7d59e9486f
HPNeo <hpneo@hotmail.com>
2013-06-06 17:43:55 -0500

    Verify authentication token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,17 @@ class UsersController < ApplicationController
   def token
     head :ok
   end
+
+  def verify_token
+    authentication_token = params[:auth_token]
+    if User.find_by_authentication_token(authentication_token).nil?
+      render json: {
+        status: 'error'
+      }
+    else
+      render json: {
+        status: 'success'
+      }
+    end
+  end
 end
